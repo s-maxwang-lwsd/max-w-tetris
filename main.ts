92c52bf2b80be57d4e0b164e6b2400e33ed01736
s-maxwang-lwsd <87514222+s-maxwang-lwsd@users.noreply.github.com>
2021-07-16 12:32:53 -0700

    FINISHED (BASICALLY)

--- main.ts
+++ main.ts
@@ -38,43 +38,43 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
             if (overlap_check()) {
                 tetrimino.setPosition(tetriminoRotationCheckX, tetriminoRotationCheckY)
                 if (tetriminoDirection == 1) {
-                    tetrimino.x += -12
-                } else if (tetriminoDirection == 2) {
-                    tetrimino.x += 6
-                } else if (tetriminoDirection == 3) {
-                    tetrimino.x += 12
-                } else if (tetriminoDirection == 4) {
                     tetrimino.x += -6
+                } else if (tetriminoDirection == 2) {
+                    tetrimino.x += -12
+                } else if (tetriminoDirection == 3) {
+                    tetrimino.x += 6
+                } else if (tetriminoDirection == 4) {
+                    tetrimino.x += 12
                 }
                 if (overlap_check()) {
                     tetrimino.setPosition(tetriminoRotationCheckX, tetriminoRotationCheckY)
                     if (tetriminoDirection == 1) {
-                        tetrimino.x += 6
-                        tetrimino.y += 12
-                    } else if (tetriminoDirection == 2) {
-                        tetrimino.x += -12
-                        tetrimino.y += 6
-                    } else if (tetriminoDirection == 3) {
-                        tetrimino.x += -6
-                        tetrimino.y += -12
-                    } else if (tetriminoDirection == 4) {
                         tetrimino.x += 12
                         tetrimino.y += -6
+                    } else if (tetriminoDirection == 2) {
+                        tetrimino.x += 6
+                        tetrimino.y += 12
+                    } else if (tetriminoDirection == 3) {
+                        tetrimino.x += -12
+                        tetrimino.y += 6
+                    } else if (tetriminoDirection == 4) {
+                        tetrimino.x += -6
+                        tetrimino.y += -12
                     }
                     if (overlap_check()) {
                         tetrimino.setPosition(tetriminoRotationCheckX, tetriminoRotationCheckY)
                         if (tetriminoDirection == 1) {
-                            tetrimino.x += -12
-                            tetrimino.y += -6
-                        } else if (tetriminoDirection == 2) {
-                            tetrimino.x += 6
-                            tetrimino.y += -12
-                        } else if (tetriminoDirection == 3) {
-                            tetrimino.x += 12
-                            tetrimino.y += 6
-                        } else if (tetriminoDirection == 4) {
                             tetrimino.x += -6
                             tetrimino.y += 12
+                        } else if (tetriminoDirection == 2) {
+                            tetrimino.x += -12
+                            tetrimino.y += -6
+                        } else if (tetriminoDirection == 3) {
+                            tetrimino.x += 6
+                            tetrimino.y += -12
+                        } else if (tetriminoDirection == 4) {
+                            tetrimino.x += 12
+                            tetrimino.y += 6
                         }
                         if (overlap_check()) {
                             tetriminoDirection += -1
@@ -88,21 +88,21 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
                 }
             }
         } else {
-            if (tetriminoDirection == 1 || tetriminoDirection == 2) {
-                tetrimino.x += -6
-            } else if (tetriminoDirection == 3 || tetriminoDirection == 4) {
+            if (tetriminoDirection == 1 || tetriminoDirection == 4) {
                 tetrimino.x += 6
+            } else if (tetriminoDirection == 2 || tetriminoDirection == 3) {
+                tetrimino.x += -6
             }
             if (overlap_check()) {
                 tetrimino.setPosition(tetriminoRotationCheckX, tetriminoRotationCheckY)
                 if (tetriminoDirection == 1) {
-                    tetrimino.x += -6
+                    tetrimino.x += 6
                     tetrimino.y += 6
                 } else if (tetriminoDirection == 2) {
                     tetrimino.x += -6
                     tetrimino.y += -6
                 } else if (tetriminoDirection == 3) {
-                    tetrimino.x += 6
+                    tetrimino.x += -6
                     tetrimino.y += 6
                 } else if (tetriminoDirection == 4) {
                     tetrimino.x += 6
@@ -111,20 +111,20 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
                 if (overlap_check()) {
                     tetrimino.setPosition(tetriminoRotationCheckX, tetriminoRotationCheckY)
                     if (tetriminoDirection == 1 || tetriminoDirection == 3) {
-                        tetrimino.y += -12
-                    } else if (tetriminoDirection == 2 || tetriminoDirection == 4) {
                         tetrimino.y += 12
+                    } else if (tetriminoDirection == 2 || tetriminoDirection == 4) {
+                        tetrimino.y += -12
                     }
                     if (overlap_check()) {
                         tetrimino.setPosition(tetriminoRotationCheckX, tetriminoRotationCheckY)
                         if (tetriminoDirection == 1) {
-                            tetrimino.x += -6
+                            tetrimino.x += 6
                             tetrimino.y += -12
                         } else if (tetriminoDirection == 2) {
                             tetrimino.x += -6
                             tetrimino.y += 12
                         } else if (tetriminoDirection == 3) {
-                            tetrimino.x += 6
+                            tetrimino.x += -6
                             tetrimino.y += -12
                         } else if (tetriminoDirection == 4) {
                             tetrimino.x += 6
@@ -282,64 +282,89 @@ function render_tetrimino () {
     if (tetriminoType == "I") {
         if (tetriminoDirection == 1) {
             tetrimino.setImage(assets.image`I-Up`)
+            tetriminoGhost.setImage(assets.image`I-Up-Ghost`)
         } else if (tetriminoDirection == 2) {
             tetrimino.setImage(assets.image`I-Right`)
+            tetriminoGhost.setImage(assets.image`I-Right-Ghost`)
         } else if (tetriminoDirection == 3) {
             tetrimino.setImage(assets.image`I-Down`)
+            tetriminoGhost.setImage(assets.image`I-Down-Ghost`)
         } else if (tetriminoDirection == 4) {
             tetrimino.setImage(assets.image`I-Left`)
+            tetriminoGhost.setImage(assets.image`I-Left-Ghost`)
         }
     } else if (tetriminoType == "O") {
         tetrimino.setImage(assets.image`O`)
+        tetriminoGhost.setImage(assets.image`O-Ghost`)
     } else if (tetriminoType == "T") {
         if (tetriminoDirection == 1) {
             tetrimino.setImage(assets.image`T-Up`)
+            tetriminoGhost.setImage(assets.image`T-Up-Ghost`)
         } else if (tetriminoDirection == 2) {
             tetrimino.setImage(assets.image`T-Right`)
+            tetriminoGhost.setImage(assets.image`T-Right-Ghost`)
         } else if (tetriminoDirection == 3) {
             tetrimino.setImage(assets.image`T-Down`)
+            tetriminoGhost.setImage(assets.image`T-Down-Ghost`)
         } else if (tetriminoDirection == 4) {
             tetrimino.setImage(assets.image`T-Left`)
+            tetriminoGhost.setImage(assets.image`T-Left-Ghost`)
         }
     } else if (tetriminoType == "S") {
         if (tetriminoDirection == 1) {
             tetrimino.setImage(assets.image`S-Up`)
+            tetriminoGhost.setImage(assets.image`S-Up-Ghost`)
         } else if (tetriminoDirection == 2) {
             tetrimino.setImage(assets.image`S-Right`)
+            tetriminoGhost.setImage(assets.image`S-Right-Ghost`)
         } else if (tetriminoDirection == 3) {
             tetrimino.setImage(assets.image`S-Down`)
+            tetriminoGhost.setImage(assets.image`S-Down-Ghost`)
         } else if (tetriminoDirection == 4) {
             tetrimino.setImage(assets.image`S-Left`)
+            tetriminoGhost.setImage(assets.image`S-Left-Ghost`)
         }
     } else if (tetriminoType == "Z") {
         if (tetriminoDirection == 1) {
             tetrimino.setImage(assets.image`Z-Up`)
+            tetriminoGhost.setImage(assets.image`Z-Up-Ghost`)
         } else if (tetriminoDirection == 2) {
             tetrimino.setImage(assets.image`Z-Right`)
+            tetriminoGhost.setImage(assets.image`Z-Right-Ghost`)
         } else if (tetriminoDirection == 3) {
             tetrimino.setImage(assets.image`Z-Down`)
+            tetriminoGhost.setImage(assets.image`Z-Down-Ghost`)
         } else if (tetriminoDirection == 4) {
             tetrimino.setImage(assets.image`Z-Left`)
+            tetriminoGhost.setImage(assets.image`Z-Left-Ghost`)
         }
     } else if (tetriminoType == "J") {
         if (tetriminoDirection == 1) {
             tetrimino.setImage(assets.image`J-Up`)
+            tetriminoGhost.setImage(assets.image`J-Up-Ghost`)
         } else if (tetriminoDirection == 2) {
             tetrimino.setImage(assets.image`J-Right`)
+            tetriminoGhost.setImage(assets.image`J-Right-Ghost`)
         } else if (tetriminoDirection == 3) {
             tetrimino.setImage(assets.image`J-Down`)
+            tetriminoGhost.setImage(assets.image`J-Down-Ghost`)
         } else if (tetriminoDirection == 4) {
             tetrimino.setImage(assets.image`J-Left`)
+            tetriminoGhost.setImage(assets.image`J-Left-Ghost`)
         }
     } else if (tetriminoType == "L") {
         if (tetriminoDirection == 1) {
             tetrimino.setImage(assets.image`L-Up`)
+            tetriminoGhost.setImage(assets.image`L-Up-Ghost`)
         } else if (tetriminoDirection == 2) {
             tetrimino.setImage(assets.image`L-Right`)
+            tetriminoGhost.setImage(assets.image`L-Right-Ghost`)
         } else if (tetriminoDirection == 3) {
             tetrimino.setImage(assets.image`L-Down`)
+            tetriminoGhost.setImage(assets.image`L-Down-Ghost`)
         } else if (tetriminoDirection == 4) {
             tetrimino.setImage(assets.image`L-Left`)
+            tetriminoGhost.setImage(assets.image`L-Left-Ghost`)
         }
     }
 }
@@ -388,6 +413,18 @@ function overlap_check () {
     }
     return 0
 }
+function ghost_overlap_check () {
+    if (tetriminoGhost.overlapsWith(edges)) {
+        return 1
+    }
+    for (let index = 0; index <= stackLocationsX.length - 1; index++) {
+        overlapCheck.setPosition(stackLocationsX[index] * 6 + 47, stackLocationsY[index] * 6 - 3)
+        if (overlapCheck.overlapsWith(tetriminoGhost)) {
+            return 1
+        }
+    }
+    return 0
+}
 function tetrimino_fallconvert () {
     tetrimino.y += 6
     if (overlap_check()) {
@@ -422,6 +459,7 @@ function tetrimino_fallconvert () {
             }
             generate_new_tetrimino()
             hardDrop = 1
+            music.pewPew.play()
         }
     }
 }
@@ -437,6 +475,7 @@ let tetriminoRotationCheckY = 0
 let tetriminoRotationCheckX = 0
 let tetriminoDirection = 0
 let hardDrop = 0
+let tetriminoGhost: Sprite = null
 let tetrimino: Sprite = null
 let bag: string[] = []
 let overlapCheck: Sprite = null
@@ -456,6 +495,7 @@ overlapCheck = sprites.create(assets.image`overlapCheck`, SpriteKind.Player)
 overlapCheck.setFlag(SpriteFlag.Invisible, true)
 bag = []
 tetrimino = sprites.create(assets.image`O`, SpriteKind.Player)
+tetriminoGhost = sprites.create(assets.image`O-Ghost`, SpriteKind.Player)
 generate_new_tetrimino()
 // Render the stack
 game.onUpdate(function () {
@@ -540,6 +580,13 @@ game.onUpdate(function () {
             index += -1
         }
     }
+})
+game.onUpdate(function () {
+    tetriminoGhost.setPosition(tetrimino.x, tetrimino.y)
+    while (!(ghost_overlap_check())) {
+        tetriminoGhost.y += 6
+    }
+    tetriminoGhost.y += -6
 })
 game.onUpdateInterval(50, function () {
     if (controller.down.isPressed()) {
